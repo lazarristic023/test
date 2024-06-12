c4d1cadbf9a6275ba6b4ccb3741cd9e4639f6bb8
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-06-12 18:25:59 +0200

    fix: data sql and missing class

--- Project/Project/src/main/resources/data.sql
+++ Project/Project/src/main/resources/data.sql
@@ -10,13 +10,7 @@ INSERT INTO users(
 	email_checked, role, id, email, password, username)
 	VALUES (false, 0, 3, 'nikolinaskiljevic@gmail.com', '$2a$12$6JZF6T2A2X0Ai9nolc5aC.ChlP2XOv8HmBg1l.8njr196/6RxiJji', 'employee2');
 
-INSERT INTO clients(
-	package_type, type, id, city, client_firm_name, client_firm_residential_address, client_surname_firmpib, country, phone)
-	VALUES (0, 0, 2, '', '', '', '', '', '');
 
-INSERT INTO clients(
-	package_type, type, id, city, client_firm_name, client_firm_residential_address, client_surname_firmpib, country, phone)
-	VALUES (0, 0, 3, '', '', '', '', '', '');
 
 INSERT INTO requests(
 	end_date, start_date, status, id, username)
